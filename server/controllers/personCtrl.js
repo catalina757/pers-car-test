e695700cab55e0377ffafbb18c5eaafc75918ce4
Catalina <catalina.condrea@ymail.com>
2021-12-19 11:30:28 +0200

    raw query - find and findAll methods personCtrl server - dotted attributes and nest option

--- server/controllers/personCtrl.js
+++ server/controllers/personCtrl.js
@@ -41,45 +41,73 @@ module.exports = db => {
     },
 
     findAll: (req, res) => {
+      db.query(`SELECT "Person"."id", "Person"."cnp", "Person"."varsta", "Person"."nume", "Person"."prenume", 
+                "Pers_Cars"."id" AS "Pers_Cars.id", "Pers_Cars"."PersonId" AS "Pers_Cars.PersonId", "Pers_Cars"."CarId" AS "Pers_Cars.CarId", 
+                "Pers_Cars->Car"."id" AS "Pers_Cars.Car.id", "Pers_Cars->Car"."marca" AS "Pers_Cars.Car.marca", "Pers_Cars->Car"."model" AS "Pers_Cars.Car.model", "Pers_Cars->Car"."an_fab" AS "Pers_Cars.Car.an_fab", "Pers_Cars->Car"."cap_cil" AS "Pers_Cars.Car.cap_cil", "Pers_Cars->Car"."imp" AS "Pers_Cars.Car.imp" 
+                FROM "Person" AS "Person" 
+                LEFT OUTER JOIN "Pers_Car" AS "Pers_Cars" 
+                ON "Person"."id" = "Pers_Cars"."PersonId" 
+                LEFT OUTER JOIN "Car" AS "Pers_Cars->Car" 
+                ON "Pers_Cars"."CarId" = "Pers_Cars->Car"."id"`, {nest: true, type: db.QueryTypes.SELECT})
 
-      db.models.Person.findAll({
-        include: [
-          {
-            model: db.models.Pers_Car,
+      // db.models.Person.findAll({
+      //   include: [
+      //     {
+      //       model: db.models.Pers_Car,
+      //
+      //       include: [
+      //         {
+      //           model: db.models.Car
+      //         }
+      //       ]
+      //     },
+      //   ],
+      //   // logging: console.log
+      // })
 
-            include: [
-              {
-                model: db.models.Car
-              }
-            ]
-          },
-        ],
-        // logging: console.log
-      }).then(resp => {
+        .then(resp => {
         res.send(resp);
+        console.log(resp[0]);
       }).catch(() => res.status(401));
     },
 
     find: (req, res) => {
-      // db.query(`SELECT id, cnp, nume, prenume, 'ceva' as x
-      // FROM "Person" WHERE id = ${req.params.id}`, { type: db.QueryTypes.SELECT})
+      // db.query(`SELECT *
+      //           FROM "Person"
+      //           INNER JOIN "Pers_Car"
+      //           ON "Person".id = "PersonId"
+      //           INNER JOIN "Car"
+      //           ON "Car".id = "CarId"`, {type: db.QueryTypes.SELECT})
 
-      db.models.Person.findOne({
-        where: {
-          id: req.params.id
-        },
-        include: [
-          {
-            model: db.models.Pers_Car,
-            include: [
-              {
-                model: db.models.Car
-              }
-            ]
-          },
-        ],
-        // logging: console.log
-      }).then(resp => {
+      db.query(` SELECT "Person"."id", "Person"."cnp", "Person"."varsta", "Person"."nume", "Person"."prenume",
+                "Pers_Cars"."id" AS "Pers_Cars.id", "Pers_Cars"."PersonId" AS "Pers_Cars.PersonId", "Pers_Cars"."CarId" AS "Pers_Cars.CarId",
+                "Pers_Cars->Car"."id" AS "Pers_Cars.Car.id", "Pers_Cars->Car"."marca" AS "Pers_Cars.Car.marca", "Pers_Cars->Car"."model" AS "Pers_Cars.Car.model", "Pers_Cars->Car"."an_fab" AS "Pers_Cars.Car.an_fab", "Pers_Cars->Car"."cap_cil" AS "Pers_Cars.Car.cap_cil", "Pers_Cars->Car"."imp" AS "Pers_Cars.Car.imp"
+                FROM "Person" AS "Person"
+                LEFT OUTER JOIN "Pers_Car" AS "Pers_Cars"
+                ON "Person"."id" = "Pers_Cars"."PersonId"
+                LEFT OUTER JOIN "Car" AS "Pers_Cars->Car"
+                ON "Pers_Cars"."CarId" = "Pers_Cars->Car"."id" WHERE "Person"."id" = ${req.params.id}`, {nest: true, type: db.QueryTypes.SELECT})
+
+      // db.models.Person.findOne({
+      //   where: {
+      //     id: req.params.id
+      //   },
+      //   include: [
+      //     {
+      //       model: db.models.Pers_Car,
+      //       include: [
+      //         {
+      //           model: db.models.Car
+      //         }
+      //       ]
+      //     },
+      //   ],
+      //   // logging: console.log
+      // })
+
+        .then(resp => {
+          // resp = JSON.stringify(resp);
+        console.log(resp);
         res.send(resp);
       }).catch(() =>
         res.status(401)
